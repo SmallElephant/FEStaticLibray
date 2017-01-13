a7f76976ad7f6ec213761741b6bbfd3b77f6e2b3
FlyElephant <keso_xiang@talkmate.com>
2017-01-13 14:46:50 +0800

    Init~

---
+++ FEStaticLibrary/FEStaticLibrary.h
@@ -0,0 +1,15 @@
+//
+//  FEStaticLibrary.h
+//  FEStaticLibrary
+//
+//  Created by FlyElephant on 17/1/13.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FEStaticLibrary : NSObject
+
+- (NSString *)make;
+
+@end
---
+++ FEStaticLibrary/FEStaticLibrary.m
@@ -0,0 +1,17 @@
+//
+//  FEStaticLibrary.m
+//  FEStaticLibrary
+//
+//  Created by FlyElephant on 17/1/13.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#import "FEStaticLibrary.h"
+
+@implementation FEStaticLibrary
+
+- (NSString *)make {
+    return @"FlyElephant";
+}
+
+@end
